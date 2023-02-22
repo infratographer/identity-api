3c152d36c788fee5477d54ed642e39288f71dd90
John Schaeffer <jschaeffer@equinix.com>
2023-02-22 21:43:36 +0000

    Add support for external CRDB instance
    Signed-off-by: John Schaeffer <jschaeffer@equinix.com>

--- internal/storage/config.go
+++ internal/storage/config.go
@@ -1,7 +1,13 @@
 package storage
 
+// CRDBConfig represents CRDB-specific configuration for identity-api.
+type CRDBConfig struct {
+	URI string
+}
+
 // Config represents the storage configuration for identity-api.
 type Config struct {
 	Type     EngineType
+	CRDB     CRDBConfig
 	SeedData SeedData
 }
--- internal/storage/engine.go
+++ internal/storage/engine.go
@@ -9,6 +9,9 @@ import (
 const (
 	// EngineTypeMemory represents an in-memory storage engine.
 	EngineTypeMemory EngineType = "memory"
+
+	// EngineTypeCRDB represents an external CockroachDB storage engine.
+	EngineTypeCRDB EngineType = "crdb"
 )
 
 // EngineType represents the type of identity-api storage engine.
@@ -36,6 +39,8 @@ func NewEngine(config Config) (Engine, error) {
 		return nil, ErrorMissingEngineType
 	case EngineTypeMemory:
 		return newMemoryEngine(config)
+	case EngineTypeCRDB:
+		return newCRDBEngine(config)
 	default:
 		err := &ErrorUnknownEngineType{
 			engineType: config.Type,
