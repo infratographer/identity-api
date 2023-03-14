c6c810be1a62a21f8d05f1840392c2fe85a0fdcb
John Schaeffer <jschaeffer@equinix.com>
2023-03-14 21:27:04 +0000

    Fix typo in const comment
    Signed-off-by: John Schaeffer <jschaeffer@equinix.com>

--- internal/types/const.go
+++ internal/types/const.go
@@ -4,6 +4,6 @@ const (
 	// URNNamespace represents the namespace for all URNs handled by identity-api.
 	URNNamespace = "infratographer"
 
-	// URNResourceTypeUbser represents the user resource type in a URN.
+	// URNResourceTypeUser represents the user resource type in a URN.
 	URNResourceTypeUser = "user"
 )
