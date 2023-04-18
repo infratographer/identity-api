mikemrm/infratographer-identity-api

swap gin for echo
Signed-off-by: Mike Mason <mimason@equinix.com>

internal/routes/oidc.go
@@ -1,10 +1,11 @@
 package routes
 
 import (
+	"net"
 	"net/http"
 	"net/url"
 
-	"github.com/gin-gonic/gin"
+	"github.com/labstack/echo/v4"
 	"go.uber.org/zap"
 )
 
@@ -22,7 +23,7 @@ type providerJSON struct {
 }
 
 // Handle processes the request for the OIDC handler.
-func (h *oidcHandler) Handle(ctx *gin.Context) {
+func (h *oidcHandler) Handle(ctx echo.Context) error {
 	out := providerJSON{
 		Issuer:      h.issuer,
 		TokenURL:    buildURL(ctx, "../../token").String(),
@@ -30,27 +31,31 @@ func (h *oidcHandler) Handle(ctx *gin.Context) {
 		UserInfoURL: buildURL(ctx, "../../userinfo").String(),
 	}
 
-	ctx.JSON(http.StatusOK, out)
+	return ctx.JSON(http.StatusOK, out)
 }
 
 // buildURL returns a new *url.URL for the current page being requested, overwriting the values with the ones provided.
 //
 //nolint:cyclop // necessary complexity.
-func buildURL(c *gin.Context, path string) *url.URL {
+func buildURL(c echo.Context, path string) *url.URL {
 	outURL := new(url.URL)
 
-	if c != nil && c.Request != nil && c.Request.URL != nil {
-		outURL.Path = c.Request.URL.JoinPath(path).Path
+	req := c.Request()
 
-		// gin doesn't expose an easy way to check if the request came from a trusted proxy.
-		// However the ClientIP will return the source ip instead of the remote ip if coming from a trusted proxy.
+	if c != nil && req != nil && req.URL != nil {
+		outURL.Path = req.URL.JoinPath(path).Path
+
+		remoteAddr, _, _ := net.SplitHostPort(req.RemoteAddr) //nolint:errcheck // we'll just use the empty string if an error occurs.
+
+		// echo doesn't expose an easy way to check if the request came from a trusted proxy.
+		// However the RealIP will return the source ip instead of the remote ip if coming from a trusted proxy.
 		// So we can compare the two, if they're the same, then we're either not behind a proxy or not behind a trusted proxy.
-		if c.ClientIP() != c.RemoteIP() {
-			if scheme := c.Request.Header.Get("X-Forwarded-Proto"); scheme != "" {
+		if c.RealIP() != remoteAddr {
+			if scheme := req.Header.Get("X-Forwarded-Proto"); scheme != "" {
 				outURL.Scheme = scheme
 			}
 
-			if host := c.Request.Header.Get("X-Forwarded-Host"); host != "" {
+			if host := req.Header.Get("X-Forwarded-Host"); host != "" {
 				outURL.Host = host
 			}
 		}
@@ -59,9 +64,9 @@ func buildURL(c *gin.Context, path string) *url.URL {
 			// Request.URL.Scheme is usually empty, however if it isn't we'll use that scheme.
 			// If empty, we'll check if TLS was used, and if so, set the scheme as https.
 			switch {
-			case c.Request.URL.Scheme != "":
-				outURL.Scheme = c.Request.URL.Scheme
-			case c.Request.TLS != nil:
+			case req.URL.Scheme != "":
+				outURL.Scheme = req.URL.Scheme
+			case req.TLS != nil:
 				outURL.Scheme = "https"
 			default:
 				outURL.Scheme = "http"
@@ -69,7 +74,7 @@ func buildURL(c *gin.Context, path string) *url.URL {
 		}
 
 		if outURL.Host == "" {
-			if host := c.Request.Host; host != "" {
+			if host := req.Host; host != "" {
 				outURL.Host = host
 			}
 		}

infratographer/identity-api

internal/routes/oidc.go
@@ -1,10 +1,11 @@
 package routes
 
 import (
+	"net"
 	"net/http"
 	"net/url"
 
-	"github.com/gin-gonic/gin"
+	"github.com/labstack/echo/v4"
 	"go.uber.org/zap"
 )
 
@@ -22,7 +23,7 @@ type providerJSON struct {
 }
 
 // Handle processes the request for the OIDC handler.
-func (h *oidcHandler) Handle(ctx *gin.Context) {
+func (h *oidcHandler) Handle(ctx echo.Context) error {
 	out := providerJSON{
 		Issuer:      h.issuer,
 		TokenURL:    buildURL(ctx, "../../token").String(),
@@ -30,30 +31,34 @@ func (h *oidcHandler) Handle(ctx *gin.Context) {
 		UserInfoURL: buildURL(ctx, "../../userinfo").String(),
 	}
 
-	ctx.JSON(http.StatusOK, out)
+	return ctx.JSON(http.StatusOK, out)
 }
 
 // buildURL returns a new *url.URL for the current page being requested, overwriting the values with the ones provided.
 //
 //nolint:cyclop // necessary complexity.
-func buildURL(c *gin.Context, path string) *url.URL {
-	if c == nil || c.Request == nil || c.Request.URL == nil {
+func buildURL(c echo.Context, path string) *url.URL {
+	if c == nil || c.Request() == nil || c.Request().URL == nil {
 		return nil
 	}
 
+	request := c.Request()
+
 	outURL := &url.URL{
-		Path: c.Request.URL.JoinPath(path).Path,
+		Path: request.URL.JoinPath(path).Path,
 	}
 
-	// gin doesn't expose an easy way to check if the request came from a trusted proxy.
-	// However the ClientIP will return the source ip instead of the remote ip if coming from a trusted proxy.
+	remoteAddr, _, _ := net.SplitHostPort(request.RemoteAddr) //nolint:errcheck // we'll just use the empty string if an error occurs.
+
+	// echo doesn't expose an easy way to check if the request came from a trusted proxy.
+	// However the RealIP will return the source ip instead of the remote ip if coming from a trusted proxy.
 	// So we can compare the two, if they're the same, then we're either not behind a proxy or not behind a trusted proxy.
-	if c.ClientIP() != c.RemoteIP() {
-		if scheme := c.Request.Header.Get("X-Forwarded-Proto"); scheme != "" {
+	if c.RealIP() != remoteAddr {
+		if scheme := request.Header.Get("X-Forwarded-Proto"); scheme != "" {
 			outURL.Scheme = scheme
 		}
 
-		if host := c.Request.Header.Get("X-Forwarded-Host"); host != "" {
+		if host := request.Header.Get("X-Forwarded-Host"); host != "" {
 			outURL.Host = host
 		}
 	}
@@ -62,9 +67,9 @@ func buildURL(c *gin.Context, path string) *url.URL {
 		// Request.URL.Scheme is usually empty, however if it isn't we'll use that scheme.
 		// If empty, we'll check if TLS was used, and if so, set the scheme as https.
 		switch {
-		case c.Request.URL.Scheme != "":
-			outURL.Scheme = c.Request.URL.Scheme
-		case c.Request.TLS != nil:
+		case request.URL.Scheme != "":
+			outURL.Scheme = request.URL.Scheme
+		case request.TLS != nil:
 			outURL.Scheme = "https"
 		default:
 			outURL.Scheme = "http"
@@ -72,7 +77,7 @@ func buildURL(c *gin.Context, path string) *url.URL {
 	}
 
 	if outURL.Host == "" {
-		if host := c.Request.Host; host != "" {
+		if host := request.Host; host != "" {
 			outURL.Host = host
 		}
 	}